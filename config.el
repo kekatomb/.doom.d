a061ec94d407cf249b681792d25adc159fbbbe8e
amadeus <190080626+am4d3us@users.noreply.github.com>
2026-02-14 07:52:28 +0100

    config: clangd for c/c++

--- config.el
+++ config.el
@@ -86,3 +86,13 @@
 
 ;; LATEX
 (setq +latex-viewers '(pdf-tools))
+
+
+;; C/C++
+(setq lsp-clients-clangd-args '("-j=3"
+				"--background-index"
+				"--clang-tidy"
+				"--completion-style=detailed"
+				"--header-insertion=never"
+				"--header-insertion-decorators=0"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
